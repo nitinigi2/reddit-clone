73efc06b6bef7f5d8968d7d6f8e68d0263acf500
Nitin Singhal <nitinsinghal139@gmail.com>
2021-09-05 13:42:45 +0530

    Add comments dto class

--- src/main/java/com/reddit/clone/dto/CommentsDto.java
+++ src/main/java/com/reddit/clone/dto/CommentsDto.java
@@ -1,4 +1,18 @@
 package com.reddit.clone.dto;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@NoArgsConstructor
+@Builder
 public class CommentsDto {
+    private Long id;
+    private Long postId;
+    private Instant createdAt;
+    private String text;
+    private String username;
 }
